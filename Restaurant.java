571475a6b1e4743dbfb447f8bb347e1b85ae8cbe
Shivani Sharma <47072878+iamshivanisharma@users.noreply.github.com>
2020-04-15 10:22:00 +0530

    Day 2

--- Restaurant.java
+++ Restaurant.java
@@ -1,8 +1,8 @@
-import java.util.*;  
+import java.util.*; 
 interface Restaurant {
     void details();
     void bill();
-    void foodChoice();
+    void output();
 }
 class getDetails implements Restaurant{
     String name;
@@ -24,8 +24,55 @@ class getDetails implements Restaurant{
         billNo++;
         
     }
+    int sum=0;
+    public void bill(){
+        Scanner in=new Scanner();
+        for(int i=0;i<price.size();i++)
+        {
+            sum+=price.get(i);
+
+        }
+        System.out.println("Applying GST on total amount: ");
+        {
+
+            sum=sum+sum*0.1;
+        }
+System.out.println("If you want to give tip enter 1 else 0");
+String s=in.next();
+if(s=="1")
+{
+    System.out.println("Enter amount to give as a tip");
+    int t=in.nextInt();
+}
+else{
+    int t=0;
+}
+int amount=sum+t;
+System.out.println("Could you please give us feedback, Yes for 1 and No for 0 ");
+String f=in.next();
+if(f=="1")
+{
+    System.out.println("Give Feedback:");
+    String fb=in.next();
+}
+    }
+public void output()
+{
+
+System.out.println("Name of the customer:"+name);
+System.out.println("Email of the customer:"+e);
+System.out.println("Contact Number of the customer:"+c);
+System.out.println("food ordered");
+for(int i=0;i<item.size();i++)
+{
+    System.out.println(item.get(i)+"\t\t"+"quantity(number of plates\t\t"+quantity.get(i));
+}
+System.out.println("Total amount paid INR"+amount+"including tip of INR"+t);
 
 }
+
+}
+
 class starter extends getDetails {
 static String [][]starter1={
                         {"Manchurian","60"},
@@ -40,12 +87,18 @@ static String [][]starter1={
 void input()
 {
     System.out.println("____STARTER____");
+    try{
     for(int i=0;i<8;i++)
      for(int j=0;j<2; j++)
      {
         System.out.println(i+"\t"+starter1[i][j] );
 
      }
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+        System.out.println(e);
+    }
     Scanner in =new Scanner(System.in);
     System.out.println("Enter the serial number of the dish: ");
     int n=in.nextInt();
@@ -54,49 +107,49 @@ void input()
     item.add(starter1[n-1][0]);
     quantity.add(q);
     price.add(q*(Integer.parseInt(starter1[n-1][1])));
-    System.out.println("Still Hungry! Try out our more delicious itmes :D");
-    System.out.println("\t1. Main Course");
-    System.out.println("\t2. Sweets");
-    System.out.println("\t3. Beverages");
-    System.out.println("\t4. Breakfast");
-    System.out.println("\t5. More from Starter");
-    System.out.println("\t0. Exit");
-    int a=in.nextInt();
-    switch(a)
-    {
-        case 1:
-        {
+//     System.out.println("Still Hungry! Try out our more delicious itmes :D");
+//     System.out.println("\t1. Main Course");
+//     System.out.println("\t2. Sweets");
+//     System.out.println("\t3. Beverages");
+//     System.out.println("\t4. Breakfast");
+//     System.out.println("\t5. More from Starter");
+//     System.out.println("\t0. Exit");
+//     int a=in.nextInt();
+//     switch(a)
+//     {
+//         case 1:
+//         {
 
-        }
-        case 2:
-        {
-
-
-        }
-        case 3:
-        {
+//         }
+//         case 2:
+//         {
 
 
-        }
-        case 4:
-        {
+//         }
+//         case 3:
+//         {
 
 
-        }
-        case 5:
-        {
+//         }
+//         case 4:
+//         {
 
 
-        }
-        case 0:
-        {
+//         }
+//         case 5:
+//         {
 
 
-        }
+//         }
+//         case 0:
+//         {
 
 
-    }
-}
+//         }
+
+
+//     }
+// }
 
 
 }
@@ -117,12 +170,18 @@ class breakfast extends getDetails
         void input()
 {
     System.out.println("____Breakfast____");
+    try{
     for(int i=0;i<9;i++)
      for(int j=0;j<2; j++)
      {
         System.out.println(i+"\t"+breakfast1[i][j] );
 
      }
+    } 
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+        System.out.println(e);
+    }
     Scanner iin =new Scanner(System.in);
     System.out.println("Enter the serial number of the dish: ");
     int n=iin.nextInt();
@@ -190,17 +249,22 @@ class mainCourse extends getDetails{
         {"Dal Makhani","100"},
         {"Veg Biryani","150"},
         {"Mix veg","90"},
-        
+         
         };
         void input()
 {
     System.out.println("____Main Course____");
+    try{
     for(int i=0;i<13;i++)
      for(int j=0;j<2; j++)
      {
         System.out.println(i+"\t"+mainCourse1[i][j] );
 
      }
+    }
+    catch(ArrayIndexOutOfBoundsException e){
+        System.out.println(e);
+    }
     Scanner iin =new Scanner(System.in);
     System.out.println("Enter the serial number of the dish: ");
     int n=iin.nextInt();
@@ -270,12 +334,17 @@ class beverages extends getDetails{
         void input()
         {
             System.out.println("____Beverages____");
+            try{
             for(int i=0;i<13;i++)
              for(int j=0;j<2; j++)
              {
                 System.out.println(i+"\t"+beverages1[i][j] );
         
              }
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+            }
             Scanner iin =new Scanner(System.in);
             System.out.println("Enter the serial number of the dish: ");
             int n=iin.nextInt();
@@ -346,27 +415,33 @@ class beverages extends getDetails{
 };
 void input()
 {
-    System.out.println("____Beverages____");
-    for(int i=0;i<13;i++)
+    System.out.println("____Sweets____");
+    try{
+    for(int i=0;i<10;i++)
      for(int j=0;j<2; j++)
      {
-        System.out.println(i+"\t"+beverages1[i][j] );
+        System.out.println(i+"\t"+sweets1[i][j] );
 
      }
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+        System.out.println(e);
+    }
     Scanner iin =new Scanner(System.in);
     System.out.println("Enter the serial number of the dish: ");
     int n=iin.nextInt();
     System.out.println("Enter the quantity (number of plates): ");
     int q=iin.nextInt();
-    item.add(mainCourse1[n-1][0]);
+    item.add(sweets1[n-1][0]);
     quantity.add(q);
-    price.add(q*(Integer.parseInt(mainCourse1[n-1][1])));
+    price.add(q*(Integer.parseInt(sweets1[n-1][1])));
     System.out.println("Still Hungry! Try out our more delicious itmes :D");
     System.out.println("\t1. Breakfast");
-    System.out.println("\t2. Sweets");
+    System.out.println("\t2. Main Course");
     System.out.println("\t3. Beverages");
     System.out.println("\t4. Starter");
-    System.out.println("\t5. More from Main Course");
+    System.out.println("\t5. More from Main sweets");
     System.out.println("\t0. Exit");
     int a=iin.nextInt();
     switch(a)
@@ -411,6 +486,6 @@ void input()
 
 
 
- }
+ 
 
 
